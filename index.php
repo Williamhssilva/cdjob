e1dc5caf7616fe949405b8060e6a029b4a703a59
William Silva <williamhss90@gmail.com>
2022-09-22 18:08:18 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+        a, a:visited {
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- ================================================================================================= -->
+    <!-- ======================================NAVBAR===================================================== -->
+
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+            <h5 class="text-white h4"><a href="#">Início</a></h5>
+            <h5 class="text-white h4"><a href="#">Cadastre-se</a></h5>
+            <h5 class="text-white h4"><a href="#">Acesso Restrito</a></h5>
+            <h5 class="text-white h4"><a href="#">Contato</a></h5>
+        </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+
+    <!-- ================================================================================================= -->
+
+    <div class="container" style="margin-top:3em;">
+        <div class="row">
+            <div class="col align-self-center">
+                <h1>CLASSIFICADOS CÓRREGO DANTA</h1><BR>
+                <h3>Ofertas de Trabalho</h3></BR>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="card" style="width: 18rem; margin-left: 2rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Secretária!</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Atendimento ao cliente</h6>
+                    <p class="card-text">Precisamos de uma pessoa bem pra trabalhar como atendento no meu escritório!.<br>
+                        <b>Salário:</b> R$ 1.800,00
+                    </p>
+                    <a href="#" class="card-link">Clique Aqui</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; margin-left: 2rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Secretária!</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Atendimento ao cliente</h6>
+                    <p class="card-text">Precisamos de uma pessoa bem pra trabalhar como atendento no meu escritório!.<br>
+                        <b>Salário:</b> R$ 1.800,00
+                    </p>
+                    <a href="#" class="card-link">Clique Aqui</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; margin-left: 2rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Secretária!</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Atendimento ao cliente</h6>
+                    <p class="card-text">Precisamos de uma pessoa bem pra trabalhar como atendento no meu escritório!.<br>
+                        <b>Salário:</b> R$ 1.800,00
+                    </p>
+                    <a href="#" class="card-link">Clique Aqui</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; margin-left: 2rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Secretária!</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Atendimento ao cliente</h6>
+                    <p class="card-text">Precisamos de uma pessoa bem pra trabalhar como atendento no meu escritório!.<br>
+                        <b>Salário:</b> R$ 1.800,00
+                    </p>
+                    <a href="#" class="card-link">Clique Aqui</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
